3298ecc75cefc26c3967c929be148f9d86b5fef6
juanbvdev <juansurfvieira123@gmail.com>
2023-09-29 20:12:20 -0300

    base concluída

---
+++ ProgramFiles/main/php/cad-med.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Cadastro de médico</title>
+</head>
+<body>
+    <a href="../html/index.html" class="menu-button2">Voltar</a>
+</body>
+</html>
